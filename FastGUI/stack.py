f9a6457a9a9983d87404d19c3c3ac225bc1ddb77
Tarou <91922753+tarou-imokenpi@users.noreply.github.com>
2024-01-12 00:17:20 +0900

    add: Stack class

--- FastGUI/stack.py
+++ FastGUI/stack.py
@@ -1,7 +1,29 @@
+import customtkinter
+
+
+class Stack:
+    """方向を指定してウィジェットを配置する"""
+
+    def __init__(self, window: customtkinter.CTk, direction: str = "v"):
+        self.window = window
+        self.instances = []
+        self.direction = ["top" if direction == "v" else "left"]
+
+    def add_stack(self, instance: object) -> object:
+        self.instances.append(instance)
+        return instance
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        [i.pack(side=self.direction) for i in self.instances]
+
+
 class Vstack:
     """垂直方向にウィジェットを配置する"""
 
-    def __init__(self, window):
+    def __init__(self, window: customtkinter.CTk):
         self.window = window
         self.instances = []
 
@@ -13,14 +35,13 @@ class Vstack:
         return self
 
     def __exit__(self, *args):
-        print("Exiting... (Vstack)")
         [i.pack(side="top") for i in self.instances]
 
 
 class Hstack:
     """垂直方向にウィジェットを配置する"""
 
-    def __init__(self, window):
+    def __init__(self, window: customtkinter.CTk):
         self.window = window
         self.instances = []
 
